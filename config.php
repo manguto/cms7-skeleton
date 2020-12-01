ab18a0e771eccdf1a9c9d0a6f9f96b56c800ca35
Marcos Torres <marcosagtorres@gmail.com>
2020-12-01 11:03:32 -0300

    Update config.php

--- config.php
+++ config.php
@@ -5,19 +5,19 @@
 // ##################################################################################### CONFIGURACOES
 // ####################################################################################################
 
-define("APP_FULL_NAME", "Controle Financeiro");
+define("APP_FULL_NAME", "Help!");
 
-define("APP_SHORT_NAME", "Controle Financeiro");
+define("APP_SHORT_NAME", "HelpApp!");
 
-define("APP_ABREV", "CFIN");
+define("APP_ABREV", "HEAP");
 
 define("APP_TITLE", APP_ABREV . " | " . APP_SHORT_NAME);
 
-define("APP_DOMAIN", "http://l.manguto.com/contas/");
+define("APP_DOMAIN", "http://doma.in/");
 
-define("APP_EMAIL", "admin@manguto.com");
+define("APP_EMAIL", "admin@doma.in");
 
-define("APP_EMAIL_ADMIN", "admin@manguto.com");
+define("APP_EMAIL_ADMIN", "admin@doma.in");
 
 // ######################################################################
 // ##################################################### BANCOS DE DADOS
